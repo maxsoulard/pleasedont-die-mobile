d57641e1790fa2a6391438904fcce212054ae91b
Maxime Soulard <max.soulard@gmail.com>
2017-07-23 01:12:45 +0200

    initialRouteName (don't know if it's useful)

--- App.js
+++ App.js
@@ -11,13 +11,18 @@ import {StatusBar, Platform} from 'react-native';
 
 // Components
 import HomeScreen from './components/HomeScreen';
+import SensorScreen from './components/SensorScreen';
 
 const Router = StackNavigator({
         Home: {screen: HomeScreen},
+        SensorScreen: {screen: SensorScreen}
     }, {
         cardStyle: {
             paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
         }
+    }, 
+    {
+        initialRouteName: 'HomeScreen',
     });
 
 export default class App extends React.Component {

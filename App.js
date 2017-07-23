0ce40e88554f188cd7428c7708b6155f5fae85ea
Maxime Soulard <max.soulard@gmail.com>
2017-07-23 17:57:18 +0200

    Duplicated import

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ import React from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import {StatusBar, Platform} from 'react-native';
-import SensorScreen from './components/SensorScreen';
 
 // Components
 import HomeScreen from './components/HomeScreen';

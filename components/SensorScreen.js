132150dbc496d131432b2c41f069d731af8971ce
Maxime Soulard <max.soulard@gmail.com>
2017-07-23 14:59:21 +0200

    Sensor parameter screen : added subscribers list

--- components/SensorScreen.js
+++ components/SensorScreen.js
@@ -16,27 +16,66 @@ export default class SensorScreen extends React.Component {
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         return (
             <View style={styles.container}>
-                <Text style={styles.title}>Adresse MAC du capteur : </Text>
-                <Text>{sensor._id}</Text>
-                <Text style={styles.title}>Abonnés aux notifications : </Text>
-                <ListView
-                    dataSource={ds.cloneWithRows(sensor.subscribers)}
-                    renderRow={(rowData) => 
-                        <View>
-                            <Text>{rowData.mail}</Text>
-                        </View>}
-                />
+                <View style={styles.informationsContainer}>
+                    <Text style={styles.title}>Adresse MAC du capteur : </Text>
+                    <Text>{sensor._id}</Text>
+                </View>
+                <View style={styles.informationsContainer}>
+                    <Text style={styles.title}>Abonnés aux notifications : </Text>
+                </View>
+                <View style={styles.subscribersList}>
+                    <ListView
+                        dataSource={ds.cloneWithRows(sensor.subscribers)}
+                        renderRow={(rowData) => 
+                            <Subscriber subscriber={rowData} />}
+                    />
+                </View>
+                <View style={styles.addContainer}>
+                    <Text style={styles.touchableAddMail}>+ Ajouter une adresse mail</Text>
+                </View>
             </View>
         );
     }
 }
 
+const Subscriber = ({subscriber}) => {
+    return (
+        <View style={styles.subscriberItem}>
+            <Text>{subscriber.mail}</Text>
+        </View>
+    )
+}
+
 const styles = StyleSheet.create({
     container: {
         flex: 1, 
-        padding: 40,
+    },
+    informationsContainer: {
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingLeft: 40,
+        paddingRight: 40,
     },
     title: {
         fontSize: 28,
+    },
+    subscribersList: {
+        backgroundColor: 'white',
+    },
+    subscriberItem: {
+        borderBottomWidth: 0.3,
+        borderColor: '#efefef',
+        paddingTop: 20,
+        paddingBottom: 20,
+        paddingLeft: 40,
+    },
+    touchableAddMail: {
+        color: 'grey',
+        paddingBottom: 100
+    },
+    addContainer: {
+        paddingTop: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
 });

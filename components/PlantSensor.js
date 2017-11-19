f93bea10167738347c5b5eafe6c7ee6cfd9258d8
maxsoulard <max.soulard@gmail.com>
2017-11-19 22:46:11 +0100

    Emojis to indicate plant status

--- components/PlantSensor.js
+++ components/PlantSensor.js
@@ -16,13 +16,22 @@ export default class Sensor extends React.Component {
     }
 
     render () {
+        let icon;
+        if (this.props.data.moisture <= 500)         icon = <Text>💧</Text>
+        else if (this.props.data.moisture <= 400)    icon = <Text>💧💧 ⚠️</Text>
+        else if (this.props.data.moisture <= 300)    icon = <Text>💧💧💧 ☠️</Text>
+        
         return (
             <TouchableHighlight style={styles.touchablecontainer} onPress={this._toSensorScreen.bind(this)}>
                 <Image
                     style={styles.plant}
                     source={require('../assets/plant.png')}>
                     <View style={styles.container}>
-                        <Text style={styles.title}>PLANTE</Text>
+                        <View style={styles.subcontainer}>
+                            <Text style={styles.title}>PLANTE</Text>
+                        </View>
+                        <View style={styles.iconcontainer}>{icon}</View>
+                        <Text style={styles.data}>Last update : {this.props.data.date}</Text>
                         <Text style={styles.data}>Valeur brute : {this.props.data.moisture}</Text>
                     </View>
                 </Image>
@@ -46,6 +55,12 @@ const styles = StyleSheet.create({
         flex: 1, 
         backgroundColor: 'rgba(0,0,0,0.6)',
         padding: 40,
+    },
+    subcontainer: {
+        flex: 0,
+        flexDirection: 'row'
+    },
+    iconcontainer: {
     },
     title: {
         color: 'white',
